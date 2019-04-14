6a94dbae886c5e00e4b8b121d43a1e95132f0fb9
Rohit Pratti <rohit.pratti@gmail.com>
2019-04-14 21:02:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function whisper(string){
   return string.toLowerCase()
 }
 function logShout(string){
-  (string).toUpperCase()
-  console.log('hello');
+ var spy = (string).toUpperCase()
+  console.log(spy);
   
 }
